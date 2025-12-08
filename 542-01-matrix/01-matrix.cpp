5789b817123e4b693b0dca4f0b2d335ea07b6472
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2025-12-08 10:19:52 +0530

    Time: 31 ms (22.59%) | Memory: 37.6 MB (49.68%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -15,25 +15,25 @@ public:
                 }
             }
         }
-        q.push({-1,-1});
-        int dist = 1;
+        // q.push({-1,-1});
+        // int dist = 1;
         while(q.size()){
             auto curr = q.front();
             q.pop();
             int i = curr.first;
             int j = curr.second;
-            if(i==-1 && j==-1){
-                if(q.size()==0) break;
-                dist++;
-                q.push({-1,-1});
-                continue;
-            }
+            // if(i==-1 && j==-1){
+            //     if(q.size()==0) break;
+            //     dist++;
+            //     q.push({-1,-1});
+            //     continue;
+            // }
             for(int d=0;d<4;d++){
                 int newrow = i + dir[d][0];
                 int newcol = j + dir[d][1];
                 if(newrow<0 || newrow>=n || newcol<0 || newcol>=m || visited[newrow][newcol]==1) continue;
                 visited[newrow][newcol] = 1;
-                ans[newrow][newcol] = dist;
+                ans[newrow][newcol] = ans[i][j] + 1;
                 q.push({newrow,newcol});
             }
         }
