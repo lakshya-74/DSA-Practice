af88fad36dbbf0b254b63e1c85396e003deaabb5
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2025-12-29 22:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.26%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> dp;
+    int n;
+    vector<int> v;
+    int rec(int level){
+        if(level>=n) return 0;
+        if(dp[level]!=-1) return dp[level];
+        int ans = v[level];
+        for(int j=2;j<n;j++){
+            int check = rec(level+j) + v[level];
+            ans = max(ans,check);
+        }
+        return dp[level] = ans;
+    }
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        v = nums;
+        dp.resize(n,-1);
+        if(n==1) return nums[0];
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = max(ans,rec(i));
+            dp.resize(n,-1);
+        }
+        return ans;
+    }
+};
