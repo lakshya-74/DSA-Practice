c951f2d3f41a967f17a818e206ae56b4e0127595
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-01-07 18:27:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (57.82%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int dp[201][201];
+    int rec(int level , int n ,int idx ,vector<vector<int>>& a){
+        if(level==n) return 0;
+        if(dp[level][idx]!=-1) return dp[level][idx];
+        int curr = a[level][idx] + rec(level+1,n,idx,a);
+        int next = a[level][idx] + rec(level+1,n,idx+1,a);
+        return dp[level][idx] = min(curr,next);
+    }
+    int minimumTotal(vector<vector<int>>& a) {
+        int n = a.size();
+        // int idx = 0;
+        // int total = a[0][0];
+        // for(int i=1;i<n;i++){
+        //     if(a[i][idx]<a[i][idx+1]){
+        //         total += a[i][idx];
+        //     }
+        //     else{
+        //         total += a[i][idx+1];
+        //         idx++;
+        //     }
+        // }
+        // return total;
+
+        // greedy ..... nhi huaa
+        // ab dp dekhte hai
+        memset(dp,-1,sizeof(dp));
+        // return rec(0,n,0,a);
+
+        // tabulation
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = a[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int x = dp[i+1][j] + a[i][j];
+                int y = dp[i+1][j+1] + a[i][j];
+                dp[i][j] = min(x,y);
+            }
+        }
+        return dp[0][0];
+    }
+};
