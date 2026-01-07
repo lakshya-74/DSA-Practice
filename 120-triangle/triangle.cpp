9bb44fbfd676a19de0c8e04328c7148b52caf6e0
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-01-07 18:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (57.82%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -28,17 +28,33 @@ public:
         memset(dp,-1,sizeof(dp));
         // return rec(0,n,0,a);
 
-        // tabulation
-        for(int j=0;j<n;j++){
-            dp[n-1][j] = a[n-1][j];
+      // -----> // tabulation
+
+
+        // for(int j=0;j<n;j++){
+        //     dp[n-1][j] = a[n-1][j];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=i;j>=0;j--){
+        //         int x = dp[i+1][j] + a[i][j];
+        //         int y = dp[i+1][j+1] + a[i][j];
+        //         dp[i][j] = min(x,y);
+        //     }
+        // }
+        // return dp[0][0];
+
+        // try it in O(n) space only
+        vector<int> arr(n,-1);
+        for(int i=0;i<n;i++){
+            arr[i] = a[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int x = dp[i+1][j] + a[i][j];
-                int y = dp[i+1][j+1] + a[i][j];
-                dp[i][j] = min(x,y);
+            for(int j=0;j<=i;j++){
+                int x = arr[j] + a[i][j];
+                int y = arr[j+1] + a[i][j];
+                arr[j] = min(x,y);
             }
         }
-        return dp[0][0];
+        return arr[0];
     }
 };
