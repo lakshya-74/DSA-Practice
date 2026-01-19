8e39456c1b97f32143697ed5b2d9168daafba122
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-01-19 16:49:47 +0530

    Time: 43 ms (39.53%) | Memory: 53 MB (12.59%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,7 +13,34 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        return rec(n-1,coins,amount,dp)==1e9 ? -1:rec(n-1,coins,amount,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        // return rec(n-1,coins,amount,dp)==1e9 ? -1:rec(n-1,coins,amount,dp);
+
+        // tabulation 
+        // steps
+        // write base case
+        // do stuffs in reverse manner as did in memo
+        // copy the recurance
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        // work as dp[idx][target]
+        // base case
+        for(int t=0;t<=amount;t++){ // at base case amount can be anything
+            if(t%coins[0]==0) dp[0][t] = t/coins[0];
+            else dp[0][t] = 1e9;
+        }
+        // coins will go from 1 to n-1 
+        // amount is from 0 to total
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                // copy the recurrence
+                int a = INT_MAX;
+                if(t-coins[i]>=0) a = 1 + dp[i][t-coins[i]];
+                int b = dp[i-1][t];
+                dp[i][t] = min(a,b);
+            }
+        }
+        int ans = dp[n-1][amount];
+        if(ans>=1e9) return -1;
+        return ans;
     }
 };
