a14a117cf2fb4110248f79a44bf3efd8f45e2df0
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-01-23 15:16:14 +0530

    Time: 39 ms (19.64%) | Memory: 27 MB (53.24%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rec(int i,int j,string& s1 , string& s2,vector<vector<int>>& dp){
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]){
+            return dp[i][j] = rec(i-1,j-1,s1,s2,dp) + rec(i-1,j,s1,s2,dp);
+        }
+        return dp[i][j] = rec(i-1,j,s1,s2,dp);
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return rec(n-1,m-1,s,t,dp);
+    }
+};
