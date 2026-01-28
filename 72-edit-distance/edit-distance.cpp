ef1564fb7b7bfd9ba20d6d1a24d75c64788d517c
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-01-28 13:18:37 +0530

    Time: 7 ms (62.06%) | Memory: 12.9 MB (79.79%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,7 +4,7 @@ public:
         if(i<0) return j+1;
         if(j<0) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]) return rec(i-1,j-1,s1,s2,dp);
+        if(s1[i]==s2[j]) return dp[i][j] = rec(i-1,j-1,s1,s2,dp);
         int a = 1 + rec(i-1,j,s1,s2,dp); // insert
         int b = 1 + rec(i,j-1,s1,s2,dp); // delete
         int c = 1 + rec(i-1,j-1,s1,s2,dp); // replace
