844f981c6121e7efc9cfe829013f29d73fbd8642
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-01-29 18:03:02 +0530

    Time: 139 ms (51.13%) | Memory: 10.3 MB (57.02%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> visited;
+    int rec(int i ,int j , vector<vector<int>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(i<0 || j<0 ||i>=n || j>=m || grid[i][j]==0) return 0;
+        if(visited[i][j]) return 0;
+        visited[i][j] = 1;
+        int a = rec(i+1,j,grid);
+        int b = rec(i-1,j,grid);
+        int c = rec(i,j+1,grid);
+        int d = rec(i,j-1,grid);
+        visited[i][j] = 0;
+        return grid[i][j] + max(a,max(b,max(c,d)));
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        visited.resize(n,vector<int>(m,0));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==0) continue;
+                ans = max(ans,rec(i,j,grid));
+            }
+        }
+        return ans;
+    }
+};
