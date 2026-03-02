1bb01f3a66275e08d725a2f1a12c34fc6d53b40e
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-02 18:27:31 +0530

    Time: 3 ms (45.00%) | Memory: 8.1 MB (100.00%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int minCost(int n) {
-        if(n==1) return 0;
-        int a = n/2;
-        int b = n - n/2;
-        return a*b + minCost(a) + minCost(b);
+        // if(n==1) return 0;
+        // int a = n/2;
+        // int b = n - n/2;
+        // return a*b + minCost(a) + minCost(b);
+        int ans = 0;
+        while(n>1){
+            int a= 1;
+            int b = n-1;
+            ans += a*b;
+            n--;
+        }
+        return ans;
     }
 };
