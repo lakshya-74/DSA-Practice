75e82a85ff8d83ca414d5bb101f6bdca89c05bd1
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-06 13:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (93.50%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
         int n = nums.size();
-        int lsum = 0;
-        int rsum = 0;
+        // int lsum = 0;
+        // int rsum = 0;
         int ans = 0;
-        for(int i=0;i<k;i++){
-            lsum += nums[i];
+        // for(int i=0;i<k;i++){
+        //     lsum += nums[i];
+        // }
+        // ans = lsum;
+        // int ridx = n-1;
+        // for(int i=k-1;i>=0;i--){
+        //     lsum -= nums[i];
+        //     rsum += nums[ridx];
+        //     ridx--;
+        //     ans = max(ans,lsum+rsum);
+        // }
+        // return ans;
+
+        int total = 0;
+        for(int i=0;i<n;i++){
+            total += nums[i];
         }
-        ans = lsum;
-        int ridx = n-1;
-        for(int i=k-1;i>=0;i--){
-            lsum -= nums[i];
-            rsum += nums[ridx];
-            ridx--;
-            ans = max(ans,lsum+rsum);
+        int curr = 0;
+        for(int i=0;i<n-k;i++) curr += nums[i];
+        ans = total-curr;
+        int l=0,r=n-k-1;
+        while(r<n-1){
+            curr -= nums[l];
+            l++;
+            r++;
+            curr += nums[r];
+            ans = max(ans,total-curr);
         }
         return ans;
     }
