12b9f503b1d3e1d225075633fb003b2598a986c9
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-06 16:49:03 +0530

    Time: 39 ms (71.80%) | Memory: 88.8 MB (7.92%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int totalFruit(vector<int>& nums) {
         int n = nums.size();
+        // it can be interpreted as "max. length subarray with at most two diff. numbers"
         map<int,int> mpp;
         int l =0 , r = 0, ans  = 0;
         while(r<n){
@@ -11,7 +12,7 @@ public:
                 if(mpp[nums[l]]==0) mpp.erase(nums[l]);
                 l++;
             }
-            ans = max(ans,r-l+1);
+            if(mpp.size()<=2) ans = max(ans,r-l+1);
             r++;
         }
         return ans;
