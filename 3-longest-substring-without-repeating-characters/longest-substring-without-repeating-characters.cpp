133d0f8625d68354e0e83a6d5eaa28a2fbe86845
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-06 15:45:09 +0530

    Time: 8 ms (55.64%) | Memory: 12 MB (42.49%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,15 +3,35 @@ public:
     int lengthOfLongestSubstring(string s) {
         int ans = 0;
         int n = s.size();
-        if(n==1) return 1;
-        unordered_set<char> st;
-        int l = 0 , r = 0;
+
+        // did using set 
+        // T.C -> O(n+n) 
+
+        // if(n==1) return 1;
+        // unordered_set<char> st;
+        // int l = 0 , r = 0;
+        // while(r<n){
+        //     while(st.count(s[r])){
+        //         st.erase(s[l]);
+        //         l++;
+        //     }
+        //     st.insert(s[r]);
+        //     ans = max(ans,r-l+1);
+        //     r++;
+        // }
+        // return ans;
+
+        // do it with hashmap that store <char , idx>
+        map<char,int> mpp;
+        int l = 0;
+        int r = 0;
         while(r<n){
-            while(st.count(s[r])){
-                st.erase(s[l]);
-                l++;
+            if(mpp.find(s[r])!=mpp.end()){ // updating when we find duplicate
+                if(mpp[s[r]]>=l){
+                    l = mpp[s[r]]  + 1;
+                }
             }
-            st.insert(s[r]);
+            mpp[s[r]] = r;
             ans = max(ans,r-l+1);
             r++;
         }
