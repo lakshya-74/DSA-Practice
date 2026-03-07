b4607857df2afff58485d74405675627ea38fd93
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-07 14:52:30 +0530

    Time: 3 ms (96.40%) | Memory: 17.3 MB (61.60%) - LeetSync

---
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int check(string s){
+        int n = s.size();
+        int op1 = 0;
+        int op2 = 0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){ // iss position pr 0 houna hai
+                if(s[i]=='1') op1++;
+                if(s[i]=='0') op2++;
+            }
+            else{
+                if(s[i]=='0') op1++;
+                if(s[i]=='1') op2++;
+            }
+        }
+        return min(op1,op2);
+    }
+    int minFlips(string s) {
+        int n = s.size();
+        string st = s+s; // circular mai bhut kaam ka hai yee
+        int l = 0;
+        int r= 0;
+        int op1 = 0 , op2 = 0;
+        int ans = INT_MAX;
+        while(r<2*n){
+            if(r%2==0){ // iss position pr 0 houna hai
+                if(st[r]=='1') op1++;
+                if(st[r]=='0') op2++;
+            }
+            else{
+                if(st[r]=='0') op1++;
+                if(st[r]=='1') op2++;
+            }  
+            if(r-l+1==n){
+                ans = min(ans,min(op1,op2));
+                if(l%2==0){
+                    if(st[l]=='1') op1--; 
+                    else op2--;
+                }
+                else{
+                    if(st[l]=='0') op1--;
+                    else op2--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans;
+    }
+};
