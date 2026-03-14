349706ea1cb2c1b79bbaae4ee2a51e7a71aec398
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-14 23:49:16 +0530

    Time: 9 ms (5.24%) | Memory: 28.7 MB (42.91%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0 && mpp[nums[i]]==1) return nums[i];
+        }
+        return -1;
+    }
+};
