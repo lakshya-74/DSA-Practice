a4a597cb9e509aa2a1037505106960cbcca18685
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-03-25 13:21:53 +0530

    Time: 17 ms (30.94%) | Memory: 136.1 MB (24.82%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -14,23 +14,29 @@ public:
                 ver[j] += grid[i][j];
             }
         }
-        long long sum = 0;
+        vector<long long> pre(n) , suff(n);
+        pre[0] = hor[0];
+        suff[n-1] = hor[n-1];
+        for(int i=1;i<n;i++){
+            pre[i] = pre[i-1] + hor[i];
+        }
+        for(int i=n-2;i>=0;i--){
+            suff[i] = suff[i+1] + hor[i];
+        }
         for(int i=0;i<n;i++){
-            sum += hor[i];
+            if(abs(pre[i]-suff[i])==hor[i]) return true;
         }
-        long long total = 0;
-        for(int i=0;i<n-1;i++){
-            total += hor[i];
-            if(2*total == sum ) return true;
+        vector<long long> pre1(m) , suff1(m);
+        pre1[0] = ver[0];
+        suff1[m-1] = ver[m-1];
+        for(int i=1;i<m;i++){
+            pre1[i] = pre1[i-1] + ver[i];
         }
-        sum = 0;
+        for(int i=m-2;i>=0;i--){
+            suff1[i] = suff1[i+1] + ver[i];
+        }
         for(int i=0;i<m;i++){
-            sum += ver[i];
-        }
-        total =0;
-        for(int i=0;i<m;i++){
-            total += ver[i];
-            if(2*total == sum) return true;
+            if(abs(pre1[i]-suff1[i])==ver[i]) return true;
         }
         return false;
     }
