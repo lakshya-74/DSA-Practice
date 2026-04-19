2a74e779735525b0efdab437541b4d4f3865516f
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-04-20 00:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (77.47%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0;
+        int hi = n-1;
+        int ans=0;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if((n-mid)<=nums[mid]){
+                ans = n-mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        return ans;
+    }
+};
