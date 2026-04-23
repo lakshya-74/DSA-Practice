6af88346b62e59f6d7194c80ccd5617f8a9911cd
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-04-23 10:08:03 +0530

    Time: 206 ms (5.00%) | Memory: 123.3 MB (30.75%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    #define ll long long
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<ll> ans(n,0);
+        // formula we will get for left side = freq*i - (sum till i)
+        map<ll,ll> a,b; // a for freq , b for sum
+        for(int i=0;i<n;i++){
+            ans[i] += a[nums[i]]*i - (b[nums[i]]);
+            a[nums[i]]++;
+            b[nums[i]] += i;
+        }
+
+        // same for right side formula = (sum till i) - (freq*i);
+        a.clear();
+        b.clear();
+        for(int i=n-1;i>=0;i--){
+            ans[i] += (b[nums[i]]) - (a[nums[i]]*i);
+            a[nums[i]]++;
+            b[nums[i]] += i;
+        }
+        return ans;
+    }
+};
