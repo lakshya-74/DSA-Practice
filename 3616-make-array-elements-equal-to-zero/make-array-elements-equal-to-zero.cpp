023aaf0eae30f38fed336dcfce117660f8d8ae00
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-04-26 21:53:13 +0530

    Time: 4 ms (70.45%) | Memory: 21.8 MB (91.21%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -14,16 +14,30 @@ public:
     }
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0) count++;
-        }
-        int ans = 0;
+        // int count = 0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=0) count++;
+        // }
+        // int ans = 0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0){
+        //         if(check(i,nums,count,1)) ans++;
+        //         if(check(i,nums,count,-1)) ans++;
+        //     }
+        // }
+        // return ans;
+
+        // optimul...
+        int total = 0;
+        for(int i=0;i<n;i++) total += nums[i];
+        int left = 0;
+        int ans =0;
         for(int i=0;i<n;i++){
             if(nums[i]==0){
-                if(check(i,nums,count,1)) ans++;
-                if(check(i,nums,count,-1)) ans++;
+                if(2*left==total) ans += 2;
+                else if(abs(2*left-total)==1) ans += 1;
             }
+            left += nums[i];
         }
         return ans;
     }
