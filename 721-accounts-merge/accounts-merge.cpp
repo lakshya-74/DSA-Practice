de1c41ef9cea96e31b5d1b4fc9fe5253049ff840
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-05-03 09:51:22 +0530

    Time: 51 ms (34.14%) | Memory: 36.7 MB (97.54%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> parent;
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+    void Union(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            parent[b] = a;
+        }
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        parent.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+        map<string,int> mpp;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string s = accounts[i][j];
+                if(mpp.find(s)==mpp.end()){
+                    mpp[s] = i;
+                }
+                else{
+                    Union(mpp[s],i);
+                }
+            }
+        }
+        vector<vector<string>> v(n);
+        for(auto x:mpp){
+            int a = find(x.second);
+            v[a].push_back(x.first);
+        }
+        for(int i=0;i<v.size();i++){
+            sort(v[i].begin(),v[i].end());
+        }
+        vector<vector<string>> ans;
+        for(int i=0;i<v.size();i++){
+            if(!v[i].empty()){
+                v[i].insert(v[i].begin(),accounts[i][0]);
+                ans.push_back(v[i]);
+            }
+        }
+        return ans;
+    }
+};
