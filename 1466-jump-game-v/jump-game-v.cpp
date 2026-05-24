5e66cd05980a8212bffd4fa7590abef15c9dd451
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-05-24 23:55:52 +0530

    Time: 7 ms (94.19%) | Memory: 19.8 MB (27.22%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -20,13 +20,48 @@ public:
     }
     int maxJumps(vector<int>& arr, int d) {
         int n = arr.size();
-        vector<int> dp(n,-1);
         int ans = 1;
+        // vector<int> dp(n,-1);
+
+        // recur. + memoisation code ---->
+
+        // for(int i=0;i<n;i++){
+        //     if(dp[i]==-1){
+        //         ans = max(ans,rec(i,arr,d,dp));
+        //     }
+        //     else ans = max(ans,dp[i]);
+        // }
+        // return ans;
+
+
+        // more optimised...
+        // top down approach
+        // we can't directly apply that thing b/c for each idx we have to go left and right both at once we can store only left values or right 
+        // so ...
+        // if we sort the array based on values and then calculate small values first
+
+        vector<int> dp(n,0);
+        vector<pair<int,int>> v(n);
         for(int i=0;i<n;i++){
-            if(dp[i]==-1){
-                ans = max(ans,rec(i,arr,d,dp));
+            v[i] = {arr[i],i};
+        }
+        sort(v.begin(),v.end());
+
+        // now do what we are doing in our normal top-down approach
+        for(int i=0;i<n;i++){
+            int idx = v[i].second;
+            int val = v[i].first;
+            int x = 1;
+            for(int j=idx+1;j<=min(n-1,idx+d);j++){
+                if(arr[j]>=val) break;
+                else x = max(x,1 + dp[j]);
             }
-            else ans = max(ans,dp[i]);
+            for(int j=idx-1;j>=max(0,idx-d);j--){
+                if(arr[j]>=val) break;
+                else x = max(x,1 + dp[j]);
+            }
+            dp[idx] = x;
+            ans = max(ans,x);
         }
         return ans;
     }
