f1287e398b4e625b4f86f30fa7d506a55e8d4abf
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-06-21 08:46:37 +0530

    Time: 18 ms (75.98%) | Memory: 108.8 MB (5.01%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int count =0;
+        // sort(costs.begin(),costs.end());
+        // int count =0;
+        // for(int i=0;i<costs.size();i++){
+        //     if(coins>=costs[i]){
+        //         count++;
+        //         coins -= costs[i];
+        //     }
+        //     else break;
+        // }
+        // return count;
+
+        vector<int> freq(1e5+1,0);
         for(int i=0;i<costs.size();i++){
-            if(coins>=costs[i]){
-                count++;
-                coins -= costs[i];
-            }
-            else break;
+            freq[costs[i]]++;
+        }
+        int count =0;
+        for(int i=1;i<=1e5;i++){
+            if(freq[i]==0) continue;
+            int canbuy = min(freq[i],coins/i);
+            count += canbuy;
+            coins -= canbuy*i;
         }
         return count;
     }
