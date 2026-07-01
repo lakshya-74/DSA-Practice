c93e83802a8ab35673a653fe3a44980ed7ffe86d
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-07-01 13:22:29 +0530

    Time: 400 ms (64.11%) | Memory: 200.5 MB (34.93%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -4,6 +4,30 @@ public:
     #define ppp pair<int,pair<int,int>>
     vector<int> dx = {1,-1,0,0};
     vector<int> dy = {0,0,1,-1};
+    bool check(int val , vector<vector<int>>& dist){
+        queue<pp> q;
+        q.push({0,0});
+        int n = dist.size();
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        visited[0][0] = 1;
+        if(dist[0][0]<val) return false;
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            int i = curr.first;
+            int j = curr.second;
+            if(i==n-1 && j==n-1) return true;
+            for(int k=0;k<4;k++){
+                int ni = i + dx[k];
+                int nj = j + dy[k];
+                if(ni>=0 && ni<n && nj>=0 && nj<n && !visited[ni][nj] && dist[ni][nj]>=val){
+                    q.push({ni,nj});
+                    visited[ni][nj] = 1;
+                }
+            }
+        }
+        return false;
+    }
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
         if(grid[0][0]==1 || grid[n-1][n-1]==1) return 0;
@@ -36,30 +60,47 @@ public:
             }
             len++;
         }
-        vector<vector<int>> v(n,vector<int>(n,-1));
-        priority_queue<ppp> pq;
-        v[0][0] = dist[0][0];
-        pq.push({v[0][0],{0,0}});
-        while(!pq.empty()){
-            auto curr = pq.top();
-            pq.pop();
-            int x = curr.first;
-            int i = curr.second.first;
-            int j = curr.second.second;
-            if(i==n-1 && j==n-1) return x;
-            for(int k=0;k<4;k++){
-                int ni = i + dx[k];
-                int nj = j + dy[k];
-                if(ni>=0 && ni<n && nj>=0 && nj<n){
-                    int y = min(x,dist[ni][nj]);
-                    if(y>v[ni][nj]){
-                        v[ni][nj] = y;
-                        pq.push({y,{ni,nj}});
-                    }
-                }
+
+        // ---> dijsktra algo.
+
+        // vector<vector<int>> v(n,vector<int>(n,-1));
+        // priority_queue<ppp> pq;
+        // v[0][0] = dist[0][0];
+        // pq.push({v[0][0],{0,0}});
+        // while(!pq.empty()){
+        //     auto curr = pq.top();
+        //     pq.pop();
+        //     int x = curr.first;
+        //     int i = curr.second.first;
+        //     int j = curr.second.second;
+        //     if(i==n-1 && j==n-1) return x;
+        //     for(int k=0;k<4;k++){
+        //         int ni = i + dx[k];
+        //         int nj = j + dy[k];
+        //         if(ni>=0 && ni<n && nj>=0 && nj<n){
+        //             int y = min(x,dist[ni][nj]);
+        //             if(y>v[ni][nj]){
+        //                 v[ni][nj] = y;
+        //                 pq.push({y,{ni,nj}});
+        //             }
+        //         }
+        //     }
+        // }
+        // return 0;
+
+        // ---> Binary Search on ans.
+        int lo = 0;
+        int hi = len-1;
+        int ans;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(check(mid,dist)){
+                ans = mid;
+                lo = mid+1;
             }
+            else hi = mid-1;
         }
-        return 0;
+        return ans;
     }
 
 };
