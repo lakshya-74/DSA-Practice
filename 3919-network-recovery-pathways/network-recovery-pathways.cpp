eeb06fb8702aa4cb88ff5ee7da5a10625798c277
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-07-03 12:37:58 +0530

    Time: 128 ms (90.91%) | Memory: 329.7 MB (71.93%) - LeetSync

---
+++ 3919-network-recovery-pathways/network-recovery-pathways.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    #define ll long long
+    #define pp pair<int,int>
+    bool check(ll x, vector<vector<pp>>& adj,vector<int>& topo,ll k){
+        int n = adj.size();
+        vector<ll> dp(n,1e18);
+        dp[0] =0;
+        for(int i=0;i<topo.size();i++){
+            int u = topo[i];
+            if(dp[u]==1e18) continue;
+            for(auto a:adj[u]){
+                int v = a.first;
+                int w = a.second;
+                if(w>=x) dp[v] = min(dp[v],dp[u]+w);
+            }
+        }
+        return dp[n-1]<=k;
+    }
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n = online.size();
+        vector<vector<pp>> adj(n);
+        int mx = 0;
+        vector<int> indegree(n);
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            if(!online[u] || !online[v]) continue;
+            adj[u].push_back({v,w});
+            indegree[v]++;
+            mx = max(mx,w);
+        }
+        vector<int> topo;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(online[i] && indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            topo.push_back(curr);
+            for(auto x:adj[curr]){
+                int v = x.first;
+                indegree[v]--;
+                if(indegree[v]==0) q.push(v);
+            }
+        }
+        ll lo = 0;
+        ll hi = mx;
+        int ans = -1;
+        while(lo<=hi){
+            ll mid = lo + (hi-lo)/2;
+            if(check(mid,adj,topo,k)){
+                ans = mid;
+                lo = mid+1;
+            }
+            else hi = mid-1;
+        }
+        return ans;
+    }
+};
