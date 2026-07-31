236a6011eb64a37d6657d535cbf917abdc528917
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-07-31 16:11:26 +0530

    Time: 6 ms (75.61%) | Memory: 28.6 MB (15.83%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> v(26,0);
+        int n = word.size();
+        for(int i=0;i<n;i++){
+            v[word[i]-'a']++;
+        }
+        sort(v.rbegin(),v.rend());
+        int ans = 0;
+        int j= 2;
+        int freq = 1;
+        for(int i=0;i<26;i++){
+            if(v[i]>0){
+                ans += (freq*v[i]);
+                j++;
+                if(j>9){
+                    j= 2;
+                    freq++;
+                }
+            }
+        }
+        return ans;
+    }
+};
