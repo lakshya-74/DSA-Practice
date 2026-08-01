d1521acff63c851eb29c100d136b1600ab9678b9
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-08-01 10:47:33 +0530

    Time: 1 ms (46.17%) | Memory: 10.2 MB (45.39%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rec(int i, int j , vector<vector<int>>&dp , vector<int>& nums){
+        if(i==j) return dp[i][j] = nums[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int a = nums[i] - rec(i+1,j,dp,nums);
+        int b = nums[j] - rec(i,j-1,dp,nums);
+        return dp[i][j] = max(a,b);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return true;
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        return rec(0,n-1,dp,nums)>=0;
+    }
+};
