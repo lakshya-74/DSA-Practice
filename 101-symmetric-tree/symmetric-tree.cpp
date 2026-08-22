2d858844dba7d6839c714b73921b3dd6a534782f
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-08-22 16:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (72.41%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,27 +11,35 @@
  */
 class Solution {
 public:
+    // bool check(TreeNode* a , TreeNode* b){
+    //     if(a==NULL && b==NULL) return true;
+    //     if(a==NULL || b==NULL) return false;
+    //     if(a->val != b->val) return false;
+    //     return check(a->left,b->left) && check(a->right,b->right);
+    //  }
+    // TreeNode* inverse(TreeNode* root){
+    //     if(root==NULL) return root;
+    //     TreeNode* temp = root->right;
+    //     root->right = root->left;
+    //     root->left = temp;
+    //     inverse(root->left);
+    //     inverse(root->right);
+    //     return root;
+    //  }
     bool check(TreeNode* a , TreeNode* b){
-        if(a==NULL && b==NULL) return true;
-        if(a==NULL || b==NULL) return false;
+        if(!a && !b) return true;
+        if(!a || !b) return false;
         if(a->val != b->val) return false;
-        return check(a->left,b->left) && check(a->right,b->right);
-     }
-    TreeNode* inverse(TreeNode* root){
-        if(root==NULL) return root;
-        TreeNode* temp = root->right;
-        root->right = root->left;
-        root->left = temp;
-        inverse(root->left);
-        inverse(root->right);
-        return root;
-     }
+        return check(a->left,b->right) && check(a->right,b->left);
+    }
     bool isSymmetric(TreeNode* root) {
 
-        // first inverse the left side so that it becomes similar
-        // and then check for each value of left and right if its different then false
-        if(root==NULL) return true;
-        inverse(root->left);
+        // // first inverse the left side so that it becomes similar
+        // // and then check for each value of left and right if its different then false
+        // if(root==NULL) return true;
+        // inverse(root->left);
+        // return check(root->left,root->right);
+        if(!root) return true;
         return check(root->left,root->right);
     }
 };
