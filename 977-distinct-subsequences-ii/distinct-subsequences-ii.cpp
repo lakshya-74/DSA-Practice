eac34b89e2b776c8e69110dbe57fbae9f20a5fe0
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-09-08 04:48:09 +0530

    Time: 159 ms (5.04%) | Memory: 9.4 MB (52.66%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int distinctSubseqII(string s) {
+        int n = s.size();
+        vector<int> dp(n, 1);
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(s[i]!=s[j]) {
+                    dp[i] = (dp[i]+dp[j])%mod;
+                }
+            }
+            ans =(ans+dp[i])%mod;
+        }
+        return ans;
+    }
+};
