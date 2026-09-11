88a2e87786672fe2e5476ecbeeb27a0067cebbf1
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-09-11 23:07:56 +0530

    Time: 71 ms (17.35%) | Memory: 42.3 MB (98.05%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,16 +9,29 @@
  */
 class Solution {
 public:
-    bool exist(TreeNode* root, TreeNode* p){
-        if(root==NULL) return false;
-        if(root==p) return true;
-        return exist(root->left,p) || exist(root->right,p);
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p || root==q) return root;
-        else if(exist(root->left,p) && exist(root->right,q)) return root;
-        else if(exist(root->right,p) && exist(root->left,q)) return root;
-        else if(exist(root->left,p) && exist(root->left,q)) return lowestCommonAncestor(root->left,p,q);
-        else return lowestCommonAncestor(root->right,p,q);
+    // bool exist(TreeNode* root, TreeNode* p){
+    //     if(root==NULL) return false;
+    //     if(root==p) return true;
+    //     return exist(root->left,p) || exist(root->right,p);
+    // }
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    //     if(root==p || root==q) return root;
+    //     else if(exist(root->left,p) && exist(root->right,q)) return root;
+    //     else if(exist(root->right,p) && exist(root->left,q)) return root;
+    //     else if(exist(root->left,p) && exist(root->left,q)) return lowestCommonAncestor(root->left,p,q);
+    //     else return lowestCommonAncestor(root->right,p,q);
+    // }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==NULL) return NULL;
+        if(root==p){
+            return p;
+        }
+        if(root==q) return q;
+        TreeNode* a = lowestCommonAncestor(root->left,p,q);
+        TreeNode* b = lowestCommonAncestor(root->right,p,q);
+        if(a && b) return root;
+        else if(a) return a;
+        return b;
     }
 };
