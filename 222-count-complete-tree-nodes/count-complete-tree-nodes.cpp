7355829dca02036e8bb512c1a203fee040d1f7fd
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-09-13 21:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (74.03%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,8 +11,27 @@
  */
 class Solution {
 public:
+    int lheight(TreeNode* root){
+        int ans = 0;
+        while(root){
+            ans++;
+            root = root->left;
+        }
+        return ans;
+    }
+    int rheight(TreeNode* root){
+        int ans =0;
+        while(root){
+            ans++;
+            root = root->right;
+        }
+        return ans;
+    }
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
+        int leftheight = lheight(root);
+        int rightheight = rheight(root);
+        if(leftheight==rightheight) return (1<<leftheight) -1;
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
