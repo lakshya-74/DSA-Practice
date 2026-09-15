681643d0880f0264feab08d389081f55352d6879
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-09-15 13:39:31 +0530

    Time: 265 ms (46.84%) | Memory: 15.3 MB (97.14%) - LeetSync

---
+++ 4406-minimum-days-to-score-exactly-n-points/minimum-days-to-score-exactly-n-points.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    #define ll long long
+    // int rec(int n , int st,vector<vector<int>>& dp){
+    //     if(n<0){
+    //         return 1e9;
+    //     }
+    //     if(n==0) return 0;
+    //     if(dp[n][st]!=-1) return dp[n][st];
+    //     int a = rec(n-st,st+1,dp);
+    //     int b = 1e9;
+    //     if(st!=1) b = rec(n,1,dp);
+    //     return dp[n][st] = min(1+a,1+b);
+    // }
+    int minDays(int n) {
+        // int mx = (sqrt(8.0*n + 1) - 1)/2;
+        // vector<vector<int>> dp(n+1,vector<int>(mx+2,-1));
+        // return rec(n,1,dp);
+
+        vector<int> dp(n+1,1e9);
+        dp[0] = 0;
+        for(int i=1;i<=n;i++){
+            int sum =0;
+            for(int k=1;k<=i;k++){
+                sum += k;
+                if(sum>i) break;
+                int rem = i - sum;
+                if(rem==0) dp[i] = min(dp[i],k);
+                else if(dp[rem]!=1e9) dp[i] = min(dp[i],dp[rem]+1+k);
+            }
+        }
+        return dp[n];
+    }
+};
