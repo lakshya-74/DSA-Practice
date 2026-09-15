55c3d7d02e0fadfd7a45e092aa63b8e0f8583e42
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-09-15 14:36:08 +0530

    Time: 12 ms (43.29%) | Memory: 34.5 MB (63.90%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -11,31 +11,35 @@
  */
 class FindElements {
 public:
-    TreeNode* root;
+    // TreeNode* root;
+    unordered_set<int> st;
     void traverse(TreeNode* root){
         if(root==NULL) return;
         if(root->left){
             root->left->val = 2*(root->val) + 1;
+            st.insert(root->left->val);
             traverse(root->left);
         }
         if(root->right){
             root->right->val = 2*(root->val) + 2;
+            st.insert(root->right->val);
             traverse(root->right);
         }
     }
-    bool trav(TreeNode* root, int t){
-        if(root==NULL) return false;
-        if(root->val==t) return true;
-        return trav(root->left,t) || trav(root->right,t);
-    }
+    // bool trav(TreeNode* root, int t){
+    //     if(root==NULL) return false;
+    //     if(root->val==t) return true;
+    //     return trav(root->left,t) || trav(root->right,t);
+    // }
     FindElements(TreeNode* root) {
-        this->root = root;
+        // this->root = root;
         root->val = 0;
+        st.insert(0);
         traverse(root);
     }
     
     bool find(int target) {
-        return trav(root,target);
+        return st.count(target);
     }
 };
 
