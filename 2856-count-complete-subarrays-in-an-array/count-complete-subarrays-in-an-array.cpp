612ede6b5edf61964873a508cdb1947f276302c2
Lakshya Patidar <lakshyapatidar7974@gmail.com>
2026-09-26 11:53:01 +0530

    Time: 13 ms (88.12%) | Memory: 41.9 MB (61.22%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -8,14 +8,32 @@ public:
         }
         int ans = 0;
         int dist = st.size();
-        st.clear();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                st.insert(nums[j]);
-                if(st.size()==dist) ans++;
-                else if(st.size()>dist) break;
+
+        // --> brutee 
+
+        // st.clear();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         st.insert(nums[j]);
+        //         if(st.size()==dist) ans++;
+        //         else if(st.size()>dist) break;
+        //     }
+        //     st.clear();
+        // }
+
+        // --> optimal
+        // sliding window
+        map<int,int> mpp;
+        int l=0,r=0;
+        while(r<n){
+            mpp[nums[r]]++;
+            while(mpp.size()==dist){
+                ans += n-r;
+                mpp[nums[l]]--;
+                if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+                l++;
             }
-            st.clear();
+            r++;
         }
         return ans;
     }
